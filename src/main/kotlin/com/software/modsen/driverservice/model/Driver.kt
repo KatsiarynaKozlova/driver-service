f978eb0bfac4e83914dfb2642b8319987f471646
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2024-10-07 08:05:22 +0300

    refactore: add driverSex

--- src/main/kotlin/com/software/modsen/driverservice/model/Driver.kt
+++ src/main/kotlin/com/software/modsen/driverservice/model/Driver.kt
@@ -1,14 +1,6 @@
 package com.software.modsen.driverservice.model
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.OneToOne
-import jakarta.persistence.Table;
+import jakarta.persistence.*
 import org.hibernate.annotations.SQLDelete
 
 @Entity
@@ -26,7 +18,8 @@ data class Driver(
     @Column(name = "phone")
     var phone: String,
     @Column(name = "sex")
-    var sex: String,
+    @Enumerated(EnumType.STRING)
+    var sex: DriverSex,
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "car_id")
     var car: Car?,
