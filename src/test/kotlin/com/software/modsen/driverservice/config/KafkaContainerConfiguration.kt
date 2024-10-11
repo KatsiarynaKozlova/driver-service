99ef295b0b49c45c1252e595e93bc97e08c72e80
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2024-10-11 14:37:10 +0300

    fix: resolve issues

--- src/test/kotlin/com/software/modsen/driverservice/config/KafkaContainerConfiguration.kt
+++ src/test/kotlin/com/software/modsen/driverservice/config/KafkaContainerConfiguration.kt
@@ -1,14 +1,10 @@
 package com.software.modsen.driverservice.config
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
-import org.testcontainers.kafka.ConfluentKafkaContainer
 import org.testcontainers.kafka.KafkaContainer
 import org.testcontainers.utility.DockerImageName
 
