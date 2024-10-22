4e5aac2fb203125fa3215db39d4c7bf64c2487ba
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2024-10-22 12:27:06 +0300

    feat: add  coroutines

--- src/main/kotlin/com/software/modsen/driverservice/service/DriverService.kt
+++ src/main/kotlin/com/software/modsen/driverservice/service/DriverService.kt
@@ -1,18 +1,20 @@
 package com.software.modsen.driverservice.service
 
 import com.software.modsen.driverservice.dto.request.DriverForRating
-import com.software.modsen.driverservice.dto.request.DriverRequest
 import com.software.modsen.driverservice.exception.CarNotFoundException
 import com.software.modsen.driverservice.exception.DriverNotFoundException
 import com.software.modsen.driverservice.exception.EmailAlreadyExistException
 import com.software.modsen.driverservice.exception.PhoneAlreadyExistException
 import com.software.modsen.driverservice.kafka.producer.DriverProducer
-import com.software.modsen.driverservice.mapper.toDriver
 import com.software.modsen.driverservice.model.Car
 import com.software.modsen.driverservice.model.Driver
 import com.software.modsen.driverservice.repository.CarRepository
 import com.software.modsen.driverservice.repository.DriverRepository
 import com.software.modsen.driverservice.util.ExceptionMessages
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import lombok.RequiredArgsConstructor
 import org.springframework.stereotype.Service
 
@@ -23,57 +25,61 @@ class DriverService(
     private val carRepository: CarRepository,
     private val driverProducer: DriverProducer
 ) {
-    fun getDriverById(id: Long): Driver = getByIdOrElseThrow(id)
+    suspend fun getDriverById(id: Long): Driver = withContext(Dispatchers.IO) { getByIdOrElseThrow(id) }
 
-    fun getAllDrivers(): List<Driver> = driverRepository.findAll()
+    suspend fun getAllDrivers(): List<Driver> = withContext(Dispatchers.IO) { driverRepository.findAll() }
 
-    fun createDriver(carId: Long, newDriver: Driver): Driver {
+    suspend fun createDriver(carId: Long, newDriver: Driver): Driver = withContext(Dispatchers.IO) {
         preCreateValidateDriver(newDriver)
         newDriver.car = getCarByIdOrElseThrow(carId)
-        val driver: Driver = driverRepository.save(newDriver)
-        val driverForRating = DriverForRating(driver.driverId!!)
-        driverProducer.sendDriver(driverForRating)
-        return driver
+        val driver = driverRepository.save(newDriver)
+        coroutineScope {
+            launch {
+                val driverForRating = DriverForRating(driver.driverId!!)
+                driverProducer.sendDriver(driverForRating)
+            }
+        }
+        driver
     }
 
-    fun updateDriver(id: Long, updatedDriver: Driver): Driver {
+    suspend fun updateDriver(id: Long, updatedDriver: Driver): Driver = withContext(Dispatchers.IO) {
         val driverOptional: Driver = getByIdOrElseThrow(id)
         preUpdateValidateCar(updatedDriver, driverOptional)
         updatedDriver.driverId = id
-        return driverRepository.save(updatedDriver)
+        driverRepository.save(updatedDriver)
     }
 
-    fun deleteDriver(id: Long) = driverRepository.deleteById(id)
+    suspend fun deleteDriver(id: Long) = withContext(Dispatchers.IO) { driverRepository.deleteById(id) }
 
-    private fun getByIdOrElseThrow(id: Long): Driver = driverRepository.findById(id)
+    private suspend fun getByIdOrElseThrow(id: Long): Driver = driverRepository.findById(id)
         .orElseThrow { DriverNotFoundException(ExceptionMessages.DRIVER_NOT_FOUND_EXCEPTION.format(id)) }
 
-    private fun checkEmailExist(email: String) {
+    private suspend fun checkEmailExist(email: String) {
         if (driverRepository.existsByEmail(email)) {
             throw EmailAlreadyExistException(ExceptionMessages.DRIVER_WITH_EMAIL_ALREADY_EXIST_EXCEPTION.format(email))
         }
     }
 
-    private fun checkPhoneExist(phone: String) {
+    private suspend fun checkPhoneExist(phone: String) {
         if (driverRepository.existsByPhone(phone)) {
             throw PhoneAlreadyExistException(ExceptionMessages.DRIVER_WITH_PHONE_ALREADY_EXIST_EXCEPTION.format(phone))
         }
     }
 
-    private fun preCreateValidateDriver(driver: Driver) {
+    private suspend fun preCreateValidateDriver(driver: Driver) {
         checkPhoneExist(driver.phone)
         checkEmailExist(driver.email)
     }
 
-    private fun preUpdateValidateCar(driverRequest: Driver, driver: Driver) {
-        if (!driver.email.equals(driverRequest.email)) {
+    private suspend fun preUpdateValidateCar(driverRequest: Driver, driver: Driver) {
+        if (driver.email != driverRequest.email) {
             checkEmailExist(driverRequest.email)
         }
-        if (!driver.phone.equals(driverRequest.phone)) {
+        if (driver.phone != driverRequest.phone) {
             checkPhoneExist(driverRequest.phone)
         }
     }
 
-    private fun getCarByIdOrElseThrow(id: Long): Car = carRepository.findById(id)
+    private suspend fun getCarByIdOrElseThrow(id: Long): Car = carRepository.findById(id)
         .orElseThrow { CarNotFoundException(ExceptionMessages.CAR_NOT_FOUND_EXCEPTION.format(id)) }
 }
