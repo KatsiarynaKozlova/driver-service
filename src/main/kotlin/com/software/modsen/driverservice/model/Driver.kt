428a421a02b26cb0718f25dd71cb7dfd358366d1
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2024-10-22 12:26:41 +0300

    ci: rewrite imports; data class -> class

--- src/main/kotlin/com/software/modsen/driverservice/model/Driver.kt
+++ src/main/kotlin/com/software/modsen/driverservice/model/Driver.kt
@@ -1,12 +1,22 @@
 package com.software.modsen.driverservice.model
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Enumerated
+import jakarta.persistence.EnumType
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
 import org.hibernate.annotations.SQLDelete
 
 @Entity
 @Table(name = "drivers")
 @SQLDelete(sql = "UPDATE drivers SET is_deleted = true WHERE driver_id=?")
-data class Driver(
+class Driver(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "driver_id")
